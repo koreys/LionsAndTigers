47a328f2a0c92a2ee78b4532c14e8fb04b0970bf
Korey Stanley <korey@twininc.com>
2014-11-08 12:39:03 -0500

    Intial Commit

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,13 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Korey Stanley on 11/8/14.
+//  Copyright (c) 2014 Black Sand. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger {
+    
+}
--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var ageLbl: UILabel!
+    @IBOutlet weak var breddLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextButtonPressed(sender: UIBarButtonItem) {
+    }
 
 }
 
